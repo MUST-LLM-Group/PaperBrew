521c9f9bd78c3448fe0814f77c6c487607850f0c
Aiden Leong <aiden.leong@aibsd.com>
2025-03-31 18:03:35 +0800

    show Conda widget if button clicked. dismiss if click outside.

--- main.py
+++ main.py
@@ -1,21 +1,61 @@
-from textual import events
+from textual import events, log
 from textual.app import ComposeResult, App
-from textual.widgets import Header, Footer, TabbedContent, TabPane
+from textual.containers import HorizontalGroup, Grid
+from textual.screen import ModalScreen
+from textual.widgets import Header, Footer, Button, Label
 from textual_terminal import Terminal
 
 from conda import Conda
-from dashboard import DashBoard
-from huggingface import HuggingFace
-from papers import Papers
-from pip import Pip
-from terminal_panel import TerminalPanel
+
+class QuitScreen(ModalScreen[bool]):  # (1)!
+    """Screen with a dialog to quit."""
+
+    def compose(self) -> ComposeResult:
+        yield Grid(
+            Label("Are you sure you want to quit?", id="question"),
+            Button("Quit", variant="error", id="quit"),
+            Button("Cancel", variant="primary", id="cancel"),
+            id="dialog",
+        )
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "quit":
+            self.dismiss(True)
+        else:
+            self.dismiss(False)
+
+
+class CondaScreen(ModalScreen[bool]):
+
+    def compose(self) -> ComposeResult:
+        yield Conda(id="conda")
+
+    def on_click(self, event: events.Click) -> None:
+        # check if mouse click outside the modal
+        # then dismiss the modal
+        conda = self.query_one("#conda")
+        if not conda.is_mouse_over:
+            self.dismiss(True)
 
 
 class PaperReproducer(App):
     """Paper reproducer app."""
 
     CSS_PATH = "main.tcss"
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    BINDINGS = [
+        ("d", "toggle_dark", "Toggle dark mode"),
+        ("q", "request_quit", "Quit"),
+    ]
+
+    def action_request_quit(self) -> None:
+        """Action to display the quit dialog."""
+
+        def check_quit(quit: bool | None) -> None:
+            """Called when QuitScreen is dismissed."""
+            if quit:
+                self.exit()
+
+        self.push_screen(QuitScreen(), check_quit)
 
     def on_resize(self, event: events.Resize) -> None:
         # self.refresh(recompose=True)
@@ -23,11 +63,22 @@ class PaperReproducer(App):
         # self.compose()
         pass
 
+    async def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "conda_button":
+            await self.push_screen(CondaScreen())
+
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
 
+        yield HorizontalGroup(
+            Button("DashBoard", id="dashboard_button"),
+            Button("Conda", id="conda_button"),
+            Button("Pip", id="pip_button"),
+            Button("HuggingFace", id="huggingface_button"),
+            Button("Papers", id="papers_button"),
+        )
         # with TabbedContent():
         #     with TabPane("DashBoard", id="dashboard"):
         #         yield DashBoard()
