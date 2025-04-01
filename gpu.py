24b332c4d5f0a27764423a98ba3e0d3ebe7a2e78
Aiden Leong <aiden.leong@aibsd.com>
2025-04-01 21:53:50 +0800

    update GPU usage display

--- gpu.py
+++ gpu.py
@@ -34,10 +34,11 @@ class GPU(Widget):
         if self.is_nvidia_smi_installed():
             asyncio.create_task(self.get_nvidia_smi_output(1))
 
-    def is_nvidia_smi_installed(self) -> bool:# 检测是否有nvidia-smi命令
+    def is_nvidia_smi_installed(self) -> bool:  # 检测是否有nvidia-smi命令
         if self.os_type == "linux" and self.cpu_arch == "x86_64":
             try:
-                result = subprocess.run(["nvidia-smi"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+                result = subprocess.run(["nvidia-smi"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
+                                        check=True)
                 # 返回标准输出
                 output = result.stdout.strip()
                 if output.__contains__("No devices were found"):
@@ -51,15 +52,42 @@ class GPU(Widget):
             print("Unsupported OS or CPU architecture")
             return False
 
+    def csv_str_to_dict(self, csv_str):
+        # 将CSV字符串分割成行
+        lines = csv_str.strip().split('\n')
+
+        # 获取标题行和值行
+        headers = lines[0].split(', ')
+        values = lines[1].split(', ')
+
+        # 将标题和值组合成字典
+        gpu_dict = {}
+        for header, value in zip(headers, values):
+            gpu_dict[header] = value
+
+        return gpu_dict
+
     async def get_nvidia_smi_output(self, interval):
         while True:
             # run nvidia-smi command
-            result = subprocess.run(["nvidia-smi"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
-            output = result.stdout
+            csv_result = subprocess.run(["nvidia-smi",
+                                         "--query-gpu=name,temperature.gpu,fan.speed,power.draw,power.limit,memory.total,memory.used,utilization.gpu,compute_mode",
+                                         "--format=csv,nounits"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                        text=True, check=True)
+            # 整理为Python字典
+            gpu_info_dict = self.csv_str_to_dict(csv_result.stdout)
             self.query_one(Log).clear()
-            self.query_one(Log).write_line(output)
-            await asyncio.sleep(interval)
+
+            self.query_one(Log).write(f"{gpu_info_dict["name"]}\n\n")
+            self.query_one(Log).write(f"Usage: {gpu_info_dict["utilization.gpu [%]"]}%\n")
+            self.query_one(Log).write(f"TEMP: {gpu_info_dict["temperature.gpu"]}C\n")
+            self.query_one(Log).write(f"FAN: {gpu_info_dict["fan.speed [%]"]}%\n")
+            self.query_one(Log).write(
+                f"POWER: {gpu_info_dict["power.draw [W]"]}W / {gpu_info_dict["power.limit [W]"]}W\n")
+            self.query_one(Log).write(
+                f"MEM: {gpu_info_dict["memory.used [MiB]"]}MiB / {gpu_info_dict["memory.total [MiB]"]}MiB\n")
+
+            await asyncio.sleep(1)
 
     def compose(self) -> ComposeResult:
         yield Log()
-        
